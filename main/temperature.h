73a387ea18bf81237cb086b4b7c37c436ee2539f
Alexandru <163155396+casnkl@users.noreply.github.com>
2026-01-18 23:13:18 +0200

    Create temperature.h

---
+++ main/temperature.h
@@ -0,0 +1,20 @@
+#ifndef TEMPERATURE_H_
+#define TEMPERATURE_H_
+
+#include "board.h"
+#include "app.h"
+#include <stdio.h>
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MCXN947_cm33_core0.h"
+#include "fsl_debug_console.h"
+#include "fsl_lpi2c.h"
+#include "fsl_device_registers.h"
+#include "oled.h"
+#include "math.h"
+#include "leds.h"
+
+void temperatures();
+
+#endif /* TEMPERATURE_H_ */
