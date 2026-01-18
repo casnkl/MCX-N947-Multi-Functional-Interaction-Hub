395cd6c6f34208d1b9b3157b8b8c7bc3a616e188
Alexandru <163155396+casnkl@users.noreply.github.com>
2026-01-18 23:11:24 +0200

    Create light_intensity.h

---
+++ main/light_intensity.h
@@ -0,0 +1,20 @@
+#ifndef LIGHT_INTENSITY_H_
+#define LIGHT_INTENSITY_H_
+
+#include "board.h"
+#include "app.h"
+#include <stdio.h>
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MCXN947_cm33_core0.h"
+#include "fsl_debug_console.h"
+#include "fsl_lpi2c.h"
+#include "fsl_device_registers.h"
+#include "oled.h"
+#include "math.h"
+#include "leds.h"
+
+void light();
+
+#endif /* LIGHT_INTENSITY_H_ */
